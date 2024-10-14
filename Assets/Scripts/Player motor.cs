76e142571c8ac2461e90e076f50b08fe64e44ac8
cschroeder7167 <cschroeder7167@student.pps.net>
2024-10-14 11:32:16 -0700

    initial comit

---
+++ Assets/Scripts/Player motor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermotor : MonoBehaviour
+{
+    private CharecterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = getComponent<CharecterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+    
+    }
+}
