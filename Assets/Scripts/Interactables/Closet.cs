f9ef1730e629c1aeb7c684a5753039ec73f801a2
cschroeder7167 <cschroeder7167@student.pps.net>
2025-05-28 11:40:48 -0700

    badad

---
+++ Assets/Scripts/Interactables/Closet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Closet: Interactable
+{
+    [SerializeField]
+    private GameObject Door;
+    private bool doorOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Design a interaction using code here
+    protected override void Interact()
+    {
+      doorOpen = !doorOpen;
+      Door.GetComponent<Animator>().SetBool("IsOpen",doorOpen);
+    }
+
+
+}
