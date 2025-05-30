aa5f34c4a137a3c904388597685895a1c69ad121
cschroeder7167 <cschroeder7167@student.pps.net>
2025-05-30 11:39:38 -0700

    all done

---
+++ Assets/Scripts/Enemy/StateMachine.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public BaseState activeState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void ChangeState(BaseState newState){
+
+        if(activeState != null){
+
+            activeState.Exit();
+        }
+        activeState = newState;
+        if(activeState != null){
+
+            activeState.stateMachine = this;
+            activeState.Enter();
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(activeState != null){
+            activeState.Perform();
+
+        }
+    }
+}
